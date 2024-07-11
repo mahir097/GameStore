28ff620e3cc5dfc7bc2e1dfd3a4c89c3a7af9058
Mahir <mahir.demir097@gmail.com>
2024-07-11 16:23:09 +0300

    Created Dto example

--- GameStore.Api/GameStore.Api/Program.cs
+++ GameStore.Api/GameStore.Api/Program.cs
@@ -1,5 +1,14 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+
+List<GameDto> games = [
+    new (1, "The Last of Us Part II", "Action", 59.99m, new DateOnly(2020, 6, 19)),
+    new (2, "Ghost of Tsushima", "Action", 59.99m, new DateOnly(2020, 7, 17)),
+    new (3, "Cyberpunk 2077", "RPG", 59.99m, new DateOnly(2020, 12, 10)),
+];
 
 app.MapGet("/", () => "Hello World!");
 
